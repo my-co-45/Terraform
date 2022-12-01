30df8792f3ebb686ccd264d6dc9e63a2b069fad8
vishakha khadse <109782510+my-co-45@users.noreply.github.com>
2022-12-01 16:00:22 +0530

    Update iam-user.tf

--- iam-user.tf
+++ iam-user.tf
@@ -1,5 +1,5 @@
-# resource "aws_iam_user" "example" {
-#   count = length(var.user_names)
-#   name  = var.user_names[count.index]
-# }
+resource "aws_iam_user" "example" {
+  count = length(var.user_names)
+  name  = var.user_names[count.index]
+}
 
